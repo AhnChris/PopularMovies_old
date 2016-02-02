69c1d5fb7c7dd083ba669748c2a85a517f4a4314
Christopher Ahn <ahn.chris1@gmail.com>
2016-02-01 22:54:22 -0800

    refactored Path as an interface and added /# to the InexactContentUri

--- app/src/main/java/com/chrisahn/popularmovies/data/FavoriteProvider.java
+++ app/src/main/java/com/chrisahn/popularmovies/data/FavoriteProvider.java
@@ -17,6 +17,10 @@ public class FavoriteProvider {
     public static final String AUTHORITY = "com.chrisahn.popularmovies.data.FavoriteProvider";
     static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
 
+    interface Path {
+        String FAVORITES = "favorites";
+    }
+
     // helper function to build Uri with BASE_CONTENT_URI
     private static Uri buildUri (String ... paths) {
         Uri.Builder builder = BASE_CONTENT_URI.buildUpon();
@@ -29,20 +33,20 @@ public class FavoriteProvider {
     @TableEndpoint(table = FavoriteDatabase.FAVORITES) public static class Favorites {
         // @ContentUri annotation is used for Uri that do not change
         @ContentUri(
-                path = "favorites",
+                path = Path.FAVORITES,
                 type = "vnd.android.cursor.dir/favorite"
         )
-        public static final Uri CONTENT_URI = buildUri("favorites");
+        public static final Uri CONTENT_URI = buildUri(Path.FAVORITES);
 
         // @InexactContentUri is used for a Uri that is created based on some kind of value
         @InexactContentUri(
-                path = "favorites",
+                path = Path.FAVORITES + "/#",
                 name = "FAVORITE_ID",
                 type = "vnd.android.cursor.item/favorite",
                 whereColumn = FavoriteColumns._ID,
                 pathSegment = 1)
         public static Uri withId (long id) {
-            return buildUri("favorites", String.valueOf(id));
+            return buildUri(Path.FAVORITES, String.valueOf(id));
         }
     }
 }
